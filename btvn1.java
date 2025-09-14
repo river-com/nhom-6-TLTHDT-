3fadf53818aeb8a93c1fa32509154346e8368512
river-com <huyn28939@gmail.com>
2025-09-14 14:00:02 +0700

    Add files via upload

---
+++ btvn1.java
@@ -0,0 +1,12 @@
+package bt;
+import java.util.Scanner;
+public class btvn {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Nhap ten cua ban: ");
+    String ten = scanner.nextLine();
+    System.out.println("Hello " + ten);
+    scanner.close();
+    }
+}
